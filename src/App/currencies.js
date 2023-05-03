3b94927aaa2beec08831c5a5461a77f28e06067f
Magda Wicik <magdaa.wicik@gmail.com>
2023-05-03 20:26:04 +0200

    currency board added

---
+++ src/App/currencies.js
@@ -0,0 +1,13 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.71,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.42,
+
+    },
+]
